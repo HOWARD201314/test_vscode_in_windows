1c1f63dc56db7bca30ad2912e3bb96e30fe77d17
LUFANGBO <LUFANGBO@kingsoft.com>
2018-11-10 16:53:25 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "file1.h"
+#include "file2.h"
+using namespace std;
+
+int main()
+{
+    string s = "hello world";
+    cout<<s<< endl;
+
+    testFile1();
+    testFile2();
+
+    char *p = (char *)malloc(100);
+    if (p == nullptr)
+    {
+        cout << "malloc failed" << endl;
+        exit(1);
+    }
+    cout << "malloc success" << endl;
+    free(p);
+
+    return 0;
+}
